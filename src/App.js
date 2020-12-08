782813c0227a59e475b0e67fd5cb7bee4b62d7d8
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-08 12:23:02 -0500

    add initial files including firebase config snippets

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">Hello Swinger!</div>;
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGXsNlBwqaJ1v4mZG0L96p3CJUnLypBp4",
+  authDomain: "zafer-store-app.firebaseapp.com",
+  projectId: "zafer-store-app",
+  storageBucket: "zafer-store-app.appspot.com",
+  messagingSenderId: "271132967364",
+  appId: "1:271132967364:web:52c4472d3be6be3126a491",
+  measurementId: "G-Y1WCG68F1Q",
+};
