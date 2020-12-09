4109f67278c976294793892c698ee12eb91e9968
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-09 10:53:26 -0500

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "../src/style/App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Switch>
+        <Route exact path={"/"} component={(Header, Home)}>
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
