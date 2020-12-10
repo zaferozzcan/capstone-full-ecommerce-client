4e855566092fd2d36fecf85bd134009e3e924f07
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-10 15:38:50 -0500

    edit payment page style

--- src/components/checkout/Accord.js
+++ src/components/checkout/Accord.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Accordion, Card, Button } from "react-bootstrap";
 import "../../style/Payment.css";
 import { useStateValue } from "../../Providers/StateProvider";
@@ -12,38 +13,52 @@ export default function Accord() {
   return (
     <div className="accord">
       <br></br>
-      <h4 className="Accord-heading">Checkout your items, {user.email}</h4>
+      <Link>
+        <h4 className="Accord-heading">Checkout Your Items, {user.email}</h4>
+      </Link>
       <div className="payment-container">
         <Accordion defaultActiveKey="0">
-          {cart.map((item, index) => {
-            return (
-              <div key={index}>
-                <Card>
-                  <Card.Header>
-                    <Accordion.Toggle
-                      as={Button}
-                      variant="link"
-                      eventKey={`${index}`}
-                    >
-                      {item.title.substring(0, 7) + "..."}
-                    </Accordion.Toggle>
-                    <button
-                      className="accordion-remove-button"
-                      onClick={() => handleRemove(item.id)}
-                    >
-                      X
-                    </button>
-                  </Card.Header>
-                  <Accordion.Collapse eventKey={`${index}`}>
-                    <Card.Body>
-                      {item.title}
-                      {item.price}
-                    </Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </div>
-            );
-          })}
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                Shipping Address
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>58 Kent Street, Quincy, MA, 02169</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Payment Method
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>Visa ending 2222</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                Review items
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="2">
+              <Card.Body>
+                {cart.length === 0
+                  ? "There is no item in the cart"
+                  : cart.map((item) => {
+                      return (
+                        <>
+                          {item.title} --{item.price}{" "}
+                          <button className="accordion-remove-button">X</button>
+                        </>
+                      );
+                    })}
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
         </Accordion>
       </div>
     </div>
