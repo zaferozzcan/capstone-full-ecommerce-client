9de4fc12d912142c47044bd5100d2f7b42998f9b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-09 20:10:11 -0500

    an issue ! cannot get the products img urls for now I have injected an empty icon but figure out later

--- src/components/checkout/CheckoutLeft.jsx
+++ src/components/checkout/CheckoutLeft.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { AiOutlineShopping } from "react-icons/ai";
+import { useStateValue } from "../../Providers/StateProvider";
 
 export default function CheckoutLeft() {
+  const [{ cart }, dispatch] = useStateValue();
   return (
     <div className="checkout-left">
       <h3 className="checkout-header">Your Items</h3>
+      {cart.map((item) => {
+        return (
+          <div style={{ border: "1px solid black", width: "80%" }}>
+            <p>{item.title}</p>
+            <div>
+              <small>${item.price}</small>
+            </div>
+            <p>{<AiOutlineShopping size={90} />}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
