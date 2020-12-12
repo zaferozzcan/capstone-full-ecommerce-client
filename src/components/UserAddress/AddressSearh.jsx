82f9cfcbf4311474cf6f011cc9d2654f032cac05
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-12 15:57:43 -0500

    Started Address search bar component, not complete yet

---
+++ src/components/UserAddress/AddressSearh.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  getGeocode,
+  usePlacesAutoComplete,
+  getLatLng,
+} from "use-places-autocomplete";
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
+// // // // // // // //
+
+export default function AddressSearh() {
+  const {} = usePlacesAutoComplete();
+  return <div></div>;
+}
