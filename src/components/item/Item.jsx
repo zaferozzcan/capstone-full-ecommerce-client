7444fd902b493a9fae2ed44c1fbddd21bae3fbb7
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-16 18:33:56 -0500

    update item card

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import SampleProduct from "../../images/sample-product.jpg";
 import "../../style/Item.css";
 import { useStateValue } from "../../Providers/StateProvider";
+import { Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
 
 export default function Item(props) {
   const [{ cart }, dispatch] = useStateValue();
@@ -18,17 +18,34 @@ export default function Item(props) {
     });
   }
   return (
-    <div className="items">
-      <img className="item-info" src={props.image} alt="" />
-      <p>{props.title.substring(0, 50) + "..."}</p>
-      <div className="item-inner">
-        <small>$</small>
-        <strong>{props.price}</strong>
-      </div>
-      <button onClick={addToCart} className="basket-button">
-        Add To Cart
-      </button>
-    </div>
+    // <div className="items">
+    //   <img className="item-info" src={props.image} alt="" />
+    //   <p>{props.title.substring(0, 50) + "..."}</p>
+    //   <div className="item-inner">
+    //     <small>$</small>
+    //     <strong>{props.price}</strong>
+    //   </div>
+    //   <button onClick={addToCart} className="basket-button">
+    //     Add To Cart
+    //   </button>
+    // </div>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Text>{props.title}</Card.Text>
+        <Card.Title>{props.price}</Card.Title>
+        {/* </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Cras justo odio</ListGroupItem>
+        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      </ListGroup>
+      <Card.Body> */}
+        <Button onClick={addToCart} href="#">
+          Add To Cart
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
 
--- src/components/item/TodaysItem.jsx
+++ src/components/item/TodaysItem.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import SampleProduct from "../../images/sample-product.jpg";
 import "../../style/Item.css";
+import { Card, ListGroup, ListGroupItem, Button } from "react-bootstrap";
 
 export default function Item() {
   return (
-    <div className="items">
-      <h1>
-        <strong>Today's Deal</strong>
-      </h1>
-      <img className="item-info" src={SampleProduct} alt="" />
-      <p>Ninja OS101 Foodi 9-in-1 Pressure Cooker and Air Fryer</p>
-      <div className="item-inner">
-        <small>$</small>
-        <strong>20.99</strong>
-      </div>
-      <button className="basket-button">See More Details</button>
-    </div>
+    <Card style={{ width: "14rem" }}>
+      <Card.Img variant="top" src />
+      <Card.Body>
+        <Card.Text>"test"</Card.Text>
+        <Card.Title>"hello"</Card.Title>
+        {/* </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroupItem>Cras justo odio</ListGroupItem>
+        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+        <ListGroupItem>Vestibulum at eros</ListGroupItem>
+      </ListGroup>
+      <Card.Body> */}
+        <Button onClick href="#">
+          Add To Cart
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
