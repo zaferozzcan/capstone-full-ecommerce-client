f73eff807b670d67cf4f08da5771d6aeea392d8c
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-15 23:38:57 -0500

    update url

--- src/components/helpChat/HelpChat.jsx
+++ src/components/helpChat/HelpChat.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+// import queryString from "query-string";
+import "../../style/HelpChat.css";
+import io from "socket.io-client";
+import { useStateValue } from "../../Providers/StateProvider";
 
-export default function HelpChat() {
-  const [userName, setUserName] = useState("");
-  const [room, setRoom] = useState("");
+let socket;
+export default function HelpChat(props) {
+  const [{ user }] = useStateValue();
+  const endPoint = "http://localhost:5001";
+  let socket;
+  useEffect(() => {
+    socket = io.connect(endPoint);
+    console.log("socket", socket);
+  }, []);
+  console.log(props.location.search);
   return (
-    <div className="join-outer-container">
-      <div className="join-inner-container">
-        <h1 className="heading">Join</h1>
-        <div>
-          <input placeholder="" className="" type="text" onChange={}></input>
-        </div>
-        <div>
-          <input placeholder="" className="" type="text" onChange={}></input>
-        </div>
-        <Link to={}>
-          <button className="button" type="submit">
-            Sign In
-          </button>
-        </Link>
-      </div>
+    <div>
+      <div id="message-container"></div>
+      <form id="send-container">
+        <input type="text" id="message-input"></input>
+        <button type="submit" id="send-button">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
